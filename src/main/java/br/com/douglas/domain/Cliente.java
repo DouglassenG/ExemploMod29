b6325f4fd3a5b83b6e98a93a702a486c88904b86
Douglas <douglasmsrodrigues@hotmail.com>
2026-05-12 10:52:31 -0300

    first commit

---
+++ src/main/java/br/com/douglas/domain/Cliente.java
@@ -0,0 +1,11 @@
+package br.com.douglas.domain;
+
+public class Cliente {
+
+        private Long id;
+        private String codigo;
+        private String nome;
+        public Long setId() {
+                return id;
+        }
+}
