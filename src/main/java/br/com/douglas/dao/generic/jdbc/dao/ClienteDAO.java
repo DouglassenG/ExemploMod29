6c7ec64b086cda6603570028329f51881013bb10
Douglas <douglasmsrodrigues@hotmail.com>
2026-05-13 13:30:14 -0300

    Criando as classes IClienteDAO e ClienteDAO

--- src/main/java/br/com/douglas/dao/generic/jdbc/dao/ClienteDAO.java
+++ src/main/java/br/com/douglas/dao/generic/jdbc/dao/ClienteDAO.java
@@ -1,5 +1,25 @@
+/**
+ * 
+ */
 package br.com.douglas.dao.generic.jdbc.dao;
 
-public class ClienteDAO {
+import java.util.List;
 
+import br.com.douglas.domain.Cliente;
+
+/**
+ * @author rodrigo.pires
+ *
+ */
+public interface ClienteDAO {
+
+	public Integer cadastrar(Cliente cliente) throws Exception;
+	
+	public Integer atualizar(Cliente cliente) throws Exception;
+	
+	public Cliente buscar(String codigo) throws Exception;
+	
+	public List<Cliente> buscarTodos() throws Exception;
+	
+	public Integer excluir(Cliente cliente) throws Exception;
 }
--- src/main/java/br/com/douglas/dao/generic/jdbc/dao/IClienteDAO.java
+++ src/main/java/br/com/douglas/dao/generic/jdbc/dao/IClienteDAO.java
@@ -1,5 +1,25 @@
+/**
+ * 
+ */
 package br.com.douglas.dao.generic.jdbc.dao;
 
+import java.util.List;
+
+import br.com.douglas.domain.Cliente;
+
+/**
+ * @author douglas
+ *
+ */
 public interface IClienteDAO {
 
+	public Integer cadastrar(Cliente cliente) throws Exception;
+	
+	public Integer atualizar(Cliente cliente) throws Exception;
+	
+	public Cliente buscar(String codigo) throws Exception;
+	
+	public List<Cliente> buscarTodos() throws Exception;
+	
+	public Integer excluir(Cliente cliente) throws Exception;
 }
